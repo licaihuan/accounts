32ad9fcbabb48e52420ee52e0bd4dd88d1b4c227
chenhuichao <mliuweidong@hotmail.com>
2015-11-21 00:26:51 +0800

    init

--- application/components/rdb/bizservice/log_svc.php
+++ application/components/rdb/bizservice/log_svc.php
@@ -1,7 +1,8 @@
 <?php
+use \rdb\integration;
+
 class LogSvc
 {/*{{{*/
-	const OBJ = 'Log';
     static $_box = array();
 
     public static function getSqlLog()
@@ -47,27 +48,55 @@ class LogSvc
             return self::$_box[$type];
         }
 
-        $fname = $_SERVER['ENV_APPLOGS_DIR'].'/'.$type.'.log';
-		if(isset($_SERVER['SHELL']))
-		{
-			$fname.= '_shell';
-		}
-	    if ( !$only )
+        $fname = Yaf_Registry::get('config')->application->log_path.'/'.$type.'.log';
+        
+    	if(isset($_SERVER['SHELL']))
+    	{
+    	    $fname.= '_shell';
+    	}
+    	if ( !$only )
         {
-            $fname.= '.'.date('Ymd');
+            $fname.= '.'.date('Ymd').'.log';
         }
-        self::$_box[$type] = new rdb\integration\LogObject( $fname );
+        self::$_box[$type] = new LogObject( $fname );
         return self::$_box[$type];
     }/*}}}*/
 
 	
-	public static function fileLog($fname,$log)
-	{
-		if(is_array($log)){
-			$log = var_export($log,TRUE);
-		}
-		$log = date('Y-m-d H:i:s')."  :\n".$log;
-		$log .= "\n";
-		file_put_contents($fname.'-'.date('Ymd').'.txt',$log,FILE_APPEND);
-	}
+    public static function fileLog($name,$log)
+    {
+    	if(is_array($log)){
+    		$log = var_export($log,TRUE);
+    	}
+    	$log = '['.date('Y-m-d H:i:s').']'.PHP_EOL.$log;
+    	$log .= PHP_EOL;
+        $fname = Yaf_Registry::get('config')->application->log_path.'/'.$name.'.log';
+    	file_put_contents($fname.'-'.date('Ymd').'.log',$log,FILE_APPEND);
+    }
+
+    public static function info($log)
+    {
+	   self::get('Info_')->log($log);
+    }
+
+    public static function warn($log)
+    {
+	   self::get('Warn_')->log($log);
+    }
+
+    public static function debug($log)
+    {
+	   self::get('Debug_')->log($log);
+    }
+
+    public static function error($log)
+    {
+        self::get('Error_')->log($log);
+    }
+
+
+
+
+
+
 }
