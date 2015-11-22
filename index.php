6e9964be1ba873d275e3a10228f7bed800a2b0a4
liuweidong <liuweidong@me-cn.(none)>
2015-11-23 02:09:36 +0800

    init

--- index.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-define('APPLICATION_PATH', dirname(__FILE__));
-require APPLICATION_PATH.'/application/vendor/autoload.php';
-$application = new Yaf_Application( APPLICATION_PATH . "/conf/application.ini");
-
-$application->bootstrap()->run();
-?>
